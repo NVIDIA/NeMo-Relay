8a2021bf557949ac53ab2ad539a0a55d20174545
David Gardner <dagardner@nvidia.com>
2026-05-15 15:24:02 -0700

    Rename 'langchain' dir to prevent 'import langchain' from accidentally suceeding
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- python/tests/integrations/conftest.py
+++ python/tests/integrations/conftest.py
@@ -5,6 +5,7 @@ import types
 
 import pytest
 
+
 @pytest.fixture(name="integration_langchain", scope='session')
 def integration_langchain_fixture() -> types.ModuleType:
     """
@@ -13,8 +14,9 @@ def integration_langchain_fixture() -> types.ModuleType:
     try:
         import langchain
         return langchain
-    except ImportError:
+    except Exception:
         pytest.skip(reason="langchain must be installed to run LangChain based tests")
+
 
 @pytest.fixture(name="integration_langgraph", scope='session')
 def integration_langgraph_fixture(integration_langchain: types.ModuleType) -> types.ModuleType:
@@ -24,7 +26,7 @@ def integration_langgraph_fixture(integration_langchain: types.ModuleType) -> ty
     try:
         import langgraph
         return langgraph
-    except ImportError:
+    except Exception:
         pytest.skip(reason="langgraph must be installed to run LangGraph based tests")
 
 
@@ -36,5 +38,5 @@ def integration_deepagents_fixture(integration_langgraph: types.ModuleType) -> t
     try:
         import deepagents
         return deepagents
-    except ImportError:
+    except Exception:
         pytest.skip(reason="deepagents must be installed to run Deep Agents based tests")
