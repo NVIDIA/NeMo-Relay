904e0e5bb7c74635ad42b2cce33e909144cb62ed
David Gardner <dagardner@nvidia.com>
2026-06-08 09:27:06 -0700

    Only set otel.status_description when the code is not error
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- crates/core/src/api/shared.rs
+++ crates/core/src/api/shared.rs
@@ -48,13 +48,26 @@ pub(crate) fn metadata_with_otel_status(
         "otel.status_code".to_string(),
         Json::String(status_code.to_string()),
     );
-    if let Some(status_message) = status_message {
-        status.insert(
-            "otel.status_description".to_string(),
-            Json::String(status_message),
-        );
+
+    // In the OTel spec, the status description should only be set if the status code is ERROR.
+    // https://opentelemetry.io/docs/specs/otel/trace/api/#set-status
+    if status_code == "ERROR" {
+        if let Some(status_message) = status_message {
+            status.insert(
+                "otel.status_description".to_string(),
+                Json::String(status_message),
+            );
+        }
     }
-    merge_json(metadata, Some(Json::Object(status)))
+    let mut metadata = merge_json(metadata, Some(Json::Object(status)));
+
+    // Explicitly remove any existing otel.status_description if the status code is not ERROR.
+    if status_code != "ERROR" {
+        if let Some(Json::Object(metadata)) = metadata.as_mut() {
+            metadata.remove("otel.status_description");
+        }
+    }
+    metadata
 }
 
 pub(crate) fn run_request_intercepts_with_codec(
